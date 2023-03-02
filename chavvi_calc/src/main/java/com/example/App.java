32f57ca9a0997ea5921dcdf302a2ce7afa81e05a
GJLoring <33187928+GJLoring@users.noreply.github.com>
2023-03-02 14:36:03 -0800

    Display
    added a display function

--- chavvi_calc/src/main/java/com/example/App.java
+++ chavvi_calc/src/main/java/com/example/App.java
@@ -9,5 +9,33 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        double a = 1.0034534535;
+        double b = 2.078989;
+        double c = 3.00;
+        drawMenu( a, b, c);
+    }
+
+    public static void drawMenu( double A, double B, double C)
+    {
+        String displayA = String.format("%.3f", A);
+        String displayB = String.format("%.3f", B);
+        String displayC = String.format("%.3f", C);
+        hLine();
+        System.out.println( "Chavvi Calc" );
+        hLine();
+        System.out.println( "A = " + displayA + "\tB = " + displayB );
+        hLine();
+        System.out.println( "a\tEnter value for A" );
+        System.out.println( "b\tEnter value for b" );
+        System.out.println( "+\tAdd" );
+        System.out.println( "-\tSubtract" );
+        System.out.println( "*\tMultiply" );
+        System.out.println( "/\tDivide" );
+        System.out.println( "c\tClear" );
+        System.out.println( "q\tQuit" );
+        hLine();
+    }
+    public static void hLine(){
+        System.out.println( "--------------------------------------------" );
     }
 }
